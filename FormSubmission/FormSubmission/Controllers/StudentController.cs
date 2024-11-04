1dddd9930c35afb1830177ec36db78043c1a5cb3
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2024-11-05 00:55:17 +0600

    formsubmission version 1

---
+++ FormSubmission/FormSubmission/Controllers/StudentController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormSubmission.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+
+        //default template
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.Name = Request["Name"];
+            ViewBag.Id = Request["Id"];
+            return View();
+        }
+        /*[HttpPost]
+        public ActionResult Create(FormCollection fc) {
+            ViewBag.Name = fc["Name"];
+            ViewBag.Id = fc["Id"];
+            return View();
+        }*/
+        [HttpPost]
+        public ActionResult Create(string Name)
+        {
+            ViewBag.Name = Name;
+
+            return View();
+        }
+    }
+}
