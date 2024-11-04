736ad4e3e8ba06592a3928526ad616f789088649
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2024-11-05 01:22:11 +0600

    added FormSubmission version 2

---
+++ FormSubmissionV2/FormSubmissionV2/Controllers/StudentController.cs
@@ -0,0 +1,46 @@
+﻿using FormSubmissionV2.Models;                                  // new import
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormSubmissionV2.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public ActionResult Create()
+        {
+
+            //ViewBag.Name = Request["Name"];
+            //ViewBag.Id = Request["Id"];
+            return View(new Student());
+        }
+        [HttpPost]
+        public ActionResult Create(Student s)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View(s);
+        }
+        /*[HttpPost]
+        public ActionResult Create(FormCollection fc) {
+            ViewBag.Name = fc["Name"];
+            ViewBag.Id = fc["Id"];
+            return View();
+        }*/
+        /*[HttpPost]
+        public ActionResult Create(string Name,int ID) {
+            ViewBag.Name = Name;
+            ViewBag.Id = ID;
+            return View();
+        }*/
+
+
+    }
+}
---
+++ FormSubmissionV2/FormSubmissionV2/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FormSubmissionV2.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage = "Please provide name")]
+        [StringLength(10)]
+        public string Name { get; set; }
+        [Required]
+        [Range(1, 40, ErrorMessage = "ID must be 1<=id<=40")]
+        public int ID { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Phone { get; set; }
+    }
+}
